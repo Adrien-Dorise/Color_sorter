689ce084d9672250c59cfad134c94a3fe41f48fd
Adrien-Dorise <adrien.dorise@hotmail.com>
2023-02-15 01:01:02 +0100

    Basic gameplay OK

--- color_sorting/Assets/Scripts/testTube.cs
+++ color_sorting/Assets/Scripts/testTube.cs
@@ -1,31 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.TerrainUtils;
+using UnityEngine.UIElements;
 
 public class testTube : MonoBehaviour
 {
     [SerializeField] private GameObject liquidPrefab;
-    [SerializeField] private int liquidNumber;
+    [SerializeField] public int initialLiquid, maxLiquid;
+    public Stack<Color> colorList = new Stack<Color>();
+    [SerializeField] int colorCount;
 
     private float yOffset = 0.475f;
+
+    private gameManager managerScript;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        for(int i = 0; i < liquidNumber; i++)
-        {
-            GameObject child = Instantiate(liquidPrefab, this.transform);
-            child.transform.localScale = new Vector3(1f, 1f / liquidNumber, 1f);
-            child.transform.position = new Vector3(0f, - yOffset + yOffset/liquidNumber + (i * 2 * yOffset / liquidNumber), 0f);
-            child.transform.GetComponent<SpriteRenderer>().color = Random.ColorHSV();
+        initialLiquid = 4;
+        maxLiquid = 6;
+        managerScript = GameObject.Find("Game Manager").GetComponent<gameManager>();
 
+        for(int i = 0; i < initialLiquid; i++)
+        {
+            addColorLayer(managerScript.colors[Random.Range(0,managerScript.colors.Count())]);
         }
+
+    }
+
+
+    public void addColorLayer(Color color)
+    {
+        GameObject child = Instantiate(liquidPrefab, this.transform);
+        child.transform.localScale = new Vector3(1f, 1f / maxLiquid, 1f);
+        child.transform.localPosition = new Vector3(0f, -yOffset + (yOffset / maxLiquid) + (colorList.Count * 2 * yOffset / maxLiquid), 0f);
+        colorList.Push(color);
+        child.transform.GetComponent<SpriteRenderer>().color = color;
+    }
+
+    public void removeColorLayer()
+    {
+        Destroy(this.transform.GetChild(colorList.Count-1).gameObject);
+        colorList.Pop();
+    }
+
+    public void click()
+    {
+        managerScript.onTubeClick(this.gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        colorCount = colorList.Count;
     }
 }
