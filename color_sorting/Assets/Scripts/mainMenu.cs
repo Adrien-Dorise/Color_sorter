6932d2c72bd134a84dff84e663a3d18b6717dbf3
Adrien-Dorise <adrien.dorise@hotmail.com>
2023-05-03 21:23:13 +0200

    Added smarter color removal for the color wheel in colorblind settings
    Similar colors are now stored in a list. Therefore, when removing a color from the texture, it is not needed to check the similarity between all pixels, but just between the few different color in the list.
    
    This list is initialised once when in the main menu
    
    Debug: remove residual color lines + avoid selection of alpha=0 colors

--- color_sorting/Assets/Scripts/mainMenu.cs
+++ color_sorting/Assets/Scripts/mainMenu.cs
@@ -15,7 +15,10 @@ public class mainMenu : MonoBehaviour
     private GameObject colorButtonsParent, backColorBlind, resetButton, dummyColorButton, dummyNewColor, wheel;
     private Color saveColor;
     private Color[] textureColor;
-
+    private List<List<int>> colorGroups;
+    private List<Color> colorGroupsIndex;
+    [SerializeField] private Sprite referenceColorWheelSprite;
+    private Texture2D colorWheelTexture;
 
     // Start is called before the first frame update
     void Start()
@@ -30,6 +33,8 @@ public class mainMenu : MonoBehaviour
         dummyColorButton = wheelCanvas.transform.GetChild(1).gameObject;
         dummyNewColor = wheelCanvas.transform.GetChild(2).gameObject;
         wheel = wheelCanvas.transform.GetChild(0).gameObject;
+        StartCoroutine(initWheelColors());
+
 
         mainCanvas.SetActive(true);
         optionsCanvas.SetActive(false);
@@ -124,23 +129,108 @@ public class mainMenu : MonoBehaviour
         dummyColorButton.GetComponent<Image>().color = colorBlindSettings.returnSavedColor(colorIndex);
         dummyNewColor.GetComponent<Image>().color = colorBlindSettings.returnSavedColor(colorIndex);
         saveColor = colorBlindSettings.returnSavedColor(colorIndex);
+        
         setWheelColors();
         wheelCanvas.SetActive(true);
     }
+
+
+    [SerializeField] List<Color> tempColor = new List<Color>();
+    private IEnumerator initWheelColors()
+    {
+        colorWheelTexture = new Texture2D(referenceColorWheelSprite.texture.width, referenceColorWheelSprite.texture.height, referenceColorWheelSprite.texture.format, false);
+        Graphics.CopyTexture(referenceColorWheelSprite.texture,colorWheelTexture);
+        wheel.GetComponent<Image>().sprite = Sprite.Create(colorWheelTexture, new Rect(0.0f, 0.0f, colorWheelTexture.width, colorWheelTexture.height), new Vector2(0.0f, 0.0f), 100.0f);
+        Debug.Log(colorWheelTexture.isReadable);
+
+        //Creation of color array to speed up forbidden color research speed.
+        colorGroupsIndex = new List<Color>();
+        colorGroups = new List<List<int>>();
+        
+        int idx = 0;
+        textureColor = wheel.GetComponent<Image>().sprite.texture.GetPixels();
+        int size = textureColor.Length;
+        bool isContained = false;
+        for(int pxl=0; pxl < size; pxl++)
+        {   
+            if(textureColor[pxl].a == 1f)
+            {
+                isContained = false;
+                foreach(Color col in colorGroupsIndex)
+                {
+                    if(isSameColor(col, textureColor[pxl]))
+                    {
+                        isContained = true;
+                        colorGroups[colorGroupsIndex.IndexOf(col)].Add(idx);
+                        break;
+                    }
+                }
+                if(!isContained)
+                {
+                    tempColor.Add(textureColor[pxl]);
+                    colorGroupsIndex.Add(textureColor[pxl]);
+                    colorGroups.Add(new List<int>());
+                    colorGroups[colorGroupsIndex.Count - 1].Add(idx);
+                }
+            }
+            else
+            {
+                textureColor[pxl] = new Color(0f,0f,0f,0f);
+            }
+            
+            if(idx %500000 == 0)
+            {
+                Debug.Log(idx + " / " + size);
+                Debug.Log(colorGroupsIndex.Count);
+                yield return new WaitForSeconds(0f);
+            }
+            idx++;
+        }
+        Debug.Log("Finished");
+    }
+
+
+    public bool isSameColor(Color colorA, Color colorB)
+    {
+        float threshold = 0.10f;
+        if(Mathf.Abs(colorA.r - colorB.r) >= threshold)
+        { return false; }
+        if(Mathf.Abs(colorA.b - colorB.b) >= threshold)
+        { return false; }
+        if(Mathf.Abs(colorA.g - colorB.g) >= threshold)
+        { return false; }
+
+        return true;
+    }
+
 
     private void setWheelColors()
     {
         List<Color> forbidenColors = new List<Color>();
         forbidenColors.AddRange(gameManager.colors);
-        textureColor = wheel.GetComponent<Image>().sprite.texture.GetPixels();
+        
+        Destroy(colorWheelTexture);
+        colorWheelTexture = new Texture2D(referenceColorWheelSprite.texture.width, referenceColorWheelSprite.texture.height, referenceColorWheelSprite.texture.format, false);
+        Graphics.CopyTexture(referenceColorWheelSprite.texture,colorWheelTexture);
+        wheel.GetComponent<Image>().sprite = Sprite.Create(colorWheelTexture, new Rect(0.0f, 0.0f, colorWheelTexture.width, colorWheelTexture.height), new Vector2(0.0f, 0.0f), 100.0f);
+        
+        textureColor = colorWheelTexture.GetPixels();
+
         Debug.Log(forbidenColors[0]);
-        for(int pxl=0; pxl < textureColor.Length; pxl++)
+        foreach(Color forbidCol in forbidenColors)
         {
-            if(forbidenColors.Exists(x => x == textureColor[pxl]))
+            foreach(Color wheelCol in colorGroupsIndex)
             {
-                textureColor[pxl] = Color.white;
+                if(isSameColor(forbidCol, wheelCol))
+                {
+                    foreach(int idx in colorGroups[colorGroupsIndex.IndexOf(wheelCol)])
+                    {
+                        textureColor[idx] = Color.white;
+                    }
+                }
             }
         }
+       
         wheel.GetComponent<Image>().sprite.texture.SetPixels(textureColor);
         wheel.GetComponent<Image>().sprite.texture.Apply();
     }
@@ -173,7 +263,6 @@ public class mainMenu : MonoBehaviour
             saveColor = newColor;
         }
         Debug.Log(newColor);
-        
     }
 
 
